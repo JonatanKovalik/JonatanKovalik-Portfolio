248aa49ede0a9c379c6d252adf9c285cf1f59950
Jonatan Kovalik <66817210+JonatanKovalik@users.noreply.github.com>
2025-06-29 00:19:04 +0300

    Add Csharp Folder

---
+++ C#/MindriX/MindriX/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace MindriX
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            button3 = new Button();
+            label3 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            label4 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 30F, FontStyle.Bold);
+            label1.Location = new Point(29, 49);
+            label1.Name = "label1";
+            label1.Size = new Size(340, 54);
+            label1.TabIndex = 0;
+            label1.Text = "Login to MindriX";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 15F);
+            label2.Location = new Point(12, 180);
+            label2.Name = "label2";
+            label2.Size = new Size(107, 28);
+            label2.TabIndex = 1;
+            label2.Text = "UserName:";
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.FromArgb(255, 128, 128);
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.Font = new Font("Arial Narrow", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button3.Location = new Point(12, 358);
+            button3.Name = "button3";
+            button3.Size = new Size(117, 44);
+            button3.TabIndex = 5;
+            button3.Text = "Quit";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 15F);
+            label3.Location = new Point(12, 270);
+            label3.Name = "label3";
+            label3.Size = new Size(97, 28);
+            label3.TabIndex = 6;
+            label3.Text = "Password:";
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(192, 255, 192);
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Arial Narrow", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.Location = new Point(270, 358);
+            button1.Name = "button1";
+            button1.Size = new Size(117, 44);
+            button1.TabIndex = 7;
+            button1.Text = "Enter";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.FromArgb(0, 192, 192);
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Font = new Font("Arial Narrow", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button2.Location = new Point(138, 358);
+            button2.Name = "button2";
+            button2.Size = new Size(117, 44);
+            button2.TabIndex = 8;
+            button2.Text = "Sign Up";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(129, 183);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(229, 23);
+            textBox1.TabIndex = 9;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(115, 270);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(229, 23);
+            textBox2.TabIndex = 10;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI Light", 12F, FontStyle.Bold);
+            label4.ForeColor = Color.Red;
+            label4.Location = new Point(12, 142);
+            label4.Name = "label4";
+            label4.Size = new Size(344, 21);
+            label4.TabIndex = 11;
+            label4.Text = "Error UserName or the Password not correct";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(399, 414);
+            Controls.Add(label4);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label3);
+            Controls.Add(button3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Login";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Button button3;
+        private Label label3;
+        private Button button1;
+        private Button button2;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label label4;
+    }
+}
---
+++ C#/MindriX/MindriX/SiginUp.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace MindriX
+{
+    public partial class SiginUp : Form
+    {
+        public SiginUp()
+        {
+            InitializeComponent();
+            label7.Visible = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (IsValidInput())
+            {
+                Form1.UserName = textBox1.Text;
+                Form1.Password = textBox2.Text;
+                Form1.Email = textBox5.Text;
+                Form1.LastName = textBox4.Text;
+                Form1.Name = textBox3.Text;
+
+                Form1 form1 = new Form1();
+                this.Hide();
+                form1.Show();
+            }
+            else
+            {
+                label7.Visible = true;
+            }
+        }
+
+        private bool IsValidInput()
+        {
+            if (string.IsNullOrEmpty(textBox1.Text) ||
+                string.IsNullOrEmpty(textBox2.Text) ||
+                string.IsNullOrEmpty(textBox3.Text) ||
+                string.IsNullOrEmpty(textBox4.Text) ||
+                string.IsNullOrEmpty(textBox5.Text))
+            {
+                label7.Text = "Please fill in all the fields";
+                return false;
+            }
+
+            if (textBox1.TextLength < 4)
+            {
+                label7.Text = "Username needs to be at least 4 characters long";
+                return false;
+            }
+
+            if (textBox2.TextLength < 8)
+            {
+                label7.Text = "Password needs to be at least 8 characters long";
+                return false;
+            }
+
+            return true;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Form1 form1 = new Form1();
+            this.Hide();
+            form1.Show();
+        }
+    }
+}
